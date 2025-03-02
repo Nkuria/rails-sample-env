7dc49d27a12981e2579cf1c7e4c3ed645822bc80
Nkuria <nkuria01@gmail.com>
2025-03-02 12:02:54 +0300

    added item model specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: items
@@ -20,5 +22,56 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { create(:company) }
+
+  describe 'associations' do
+    it 'belongs to a company' do
+      item = described_class.reflect_on_association(:company)
+      expect(item.macro).to eq(:belongs_to)
+    end
+
+    it 'has many deals with dependent destroy' do
+      item = described_class.reflect_on_association(:deals)
+      expect(item.macro).to eq(:has_many)
+      expect(item.options[:dependent]).to eq(:destroy)
+    end
+  end
+
+  describe 'validations' do
+    it 'is invalid without a name' do
+      item = build(:item, name: nil)
+      expect(item).not_to be_valid
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is invalid without a VAT' do
+      item = build(:item, vat: nil)
+      expect(item).not_to be_valid
+      expect(item.errors[:vat]).to include("can't be blank")
+    end
+
+    it 'is invalid if VAT is below 0' do
+      item = build(:item, vat: -1)
+      expect(item).not_to be_valid
+      expect(item.errors[:vat]).to include('must be greater than or equal to 0')
+    end
+
+    it 'is invalid if VAT is above 100' do
+      item = build(:item, vat: 101)
+      expect(item).not_to be_valid
+      expect(item.errors[:vat]).to include('must be less than or equal to 100')
+    end
+
+    it 'is valid with a VAT between 0 and 100' do
+      item = build(:item, vat: 50)
+      expect(item).to be_valid
+    end
+  end
+
+  describe 'factory' do
+    it 'is valid with valid attributes' do
+      item = build(:item, company: company)
+      expect(item).to be_valid
+    end
+  end
 end
